76f8517b7f15b5ee29d6e242299ab3c4e8341f8a
Bhagyashree Rahile <bhagyashreerahile@gmail.com>
2023-06-26 15:07:28 +0530

    inital commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const AppLayout = () => {
+    return (
+        <h1>hidfdsdfdssdfd asf dgsd</h1>
+    )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<AppLayout/>);
