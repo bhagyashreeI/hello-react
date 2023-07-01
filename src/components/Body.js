d0a4148d15b154303277f784693a0cc444ddf7d9
Bhagyashree Rahile <bhagyashreerahile@gmail.com>
2023-07-01 20:23:52 +0530

    added filters

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,6 +5,7 @@ import Shimmer from './Shimmer';
 const Body = () => {
     const [restarantList,setRestList] = useState([]);
     const [filtredRestarantList,setFiltredRestList] = useState([]);
+    const [restaurantCnt,setRestaurantCnt] = useState('');
     //const [restarantList, setRestList] = useState({previous: [], current: []});
     const [searchval,setSearchVal] = useState('');
     useEffect(()=>{
@@ -16,11 +17,14 @@ const Body = () => {
         const allrest = json?.data?.cards?.[2]?.data?.data?.cards;
         setRestList(allrest);
         setFiltredRestList(allrest);
+        setRestaurantCnt(allrest.length)
     }
+    const filterBy = (type,order=null) =>{
+        const filteredList = [...restarantList].sort((a, b) => parseInt(a.data.deliveryTime) - parseInt(b.data.deliveryTime));
+        setFiltredRestList(filteredList);
+    }
+    console.log("rendering");
     //conditional rendering
-    
-   
-
     return filtredRestarantList.length === 0 ? 
         <div className="shimmer-container">
             {Array(10).fill(1).map((el, i) =>
@@ -31,10 +35,12 @@ const Body = () => {
             <div className="filter">
                 <button className="filter-btn body-btn" onClick={()=>{
                     setFiltredRestList(restarantList)
+                    setRestaurantCnt(restarantList.length)
                 }}>All Restaurants</button>
                 <button className="filter-btn body-btn" onClick={()=>{
                     const filteredList =  restarantList.filter((res) => res.data.avgRating > 4);
                     setFiltredRestList(filteredList);
+                    setRestaurantCnt(filteredList.length)
                 }}>Top Rated Restaurants</button>
                 <input type="text" onChange={(e)=>{
                     setSearchVal(e.target.value)
@@ -42,15 +48,46 @@ const Body = () => {
                 <button className="search-btn body-btn" onClick={() => {
                     if(searchval == ''){
                         setFiltredRestList(restarantList)
+                        setRestaurantCnt(restarantList.length)
                     }else{
                         const filteredList =  restarantList.filter((res) => res?.data?.name?.toLowerCase()?.includes(searchval.toLowerCase()));
                         console.log(filteredList);
                         setFiltredRestList(filteredList);
+                        setRestaurantCnt(filteredList.length)
                     }
                     
                 }}>Search</button>
             </div>
+                <div className="main-filter-section">
+                <div>
+                    <div className="head">{restaurantCnt} restaurants</div>
+                </div>
+                <div className="inner-filters-container">
+                    <div className="inner-filters" onClick={()=>{
+                            const filteredList = [...restarantList].sort((a, b) => parseInt(a?.data?.deliveryTime) - parseInt(b?.data?.deliveryTime));
+                            
+                            setFiltredRestList(filteredList);
+                    }}>Delivery Time</div>
+                    <div className="inner-filters" onClick={()=>{
+                            const filteredList = restarantList.sort((a, b) => parseFloat(b?.data?.avgRating) -  parseFloat(a?.data?.avgRating))
+                            
+                            setFiltredRestList(filteredList);
+                    }}>Rating</div>
+                        <div className="inner-filters" onClick={() => {
+                            const filteredList = [...restarantList].sort((a, b) => parseFloat(a.data.costForTwo) - parseFloat(b.data.costForTwo));
+                            
+                            setFiltredRestList(filteredList);
+                        }}>Cost: Low to High</div>
+                    <div className="inner-filters" onClick={()=>{
+                            const filteredList = [...restarantList].sort((a, b) => parseFloat(b.data.costForTwo) - parseFloat(a.data.costForTwo));
+                            
+                            setFiltredRestList(filteredList);
+                    }}>Cost: High to Low</div>
+                </div>
+            </div>
+            
             <div className='container'>
+                
                 {
                     filtredRestarantList.map(restaurant =>(
                         <RestCard key={restaurant.data.id} restData={restaurant} />
